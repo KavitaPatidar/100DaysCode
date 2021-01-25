9f4375b3e144adb8bab70c248b2a4ff815e7d47e
Kavita <patidar.ka@husky.neu.edu>
2021-01-25 11:03:36 -0500

    for_practice

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-enemies=1
 
-def increase_enemy():
-    print(f" increased enemies are {enemies}")
-    return enemies+1
-
-enemies =increase_enemy()
-print(f" increased enemies are {enemies}")
 
 
 
